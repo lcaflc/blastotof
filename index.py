d3c23776a64736444535948eee29e1a8333788d9
Laurent Corbes <caf@glot.net>
2008-10-21 09:26:57 +0200

    correct commit for table cleanup code

--- index.py
+++ index.py
@@ -3,14 +3,13 @@
 import os,sys,string,re,urllib
 import Image
 
-debug       = 0
-version     = "2.0"
+debug       = False
+version     = "3.0"
 title       = "Blastotof"
 
 stylefile   = "style.css"
 readmefile  = "TITLE.txt"
-descfile    = "DESC.txt"
-cachedir    = ".blastotof"
+cachedir    = ".cache"
 
 dirlist     = []
 toflist     = []
@@ -70,56 +69,61 @@ def choose_style_file():
 def print_headers():
     print "<html>\n<head>\n<title>%s</title>"%(title)
     choose_style_file()
-    print "</head><body>"
-    print "<table width=100% align=left>"
+    print """</head><body>
+    <div id='global'>
+    """
     
 
 def show_title():
     # get and print the readme file
     if os.path.isfile(os.path.join(path, readmefile)):
-        print "<tr><td></td><td class='title-td'><h1 class='title-text'>"
-        print open(os.path.join(path, readmefile)).read()
-        print "</h1></td></tr>"
-    print "<tr colspan=2><td colspan=2 class='menu-td' width='100%' align='center'>"
-    print path#"&nbsp"
+        print """
+        <div id='title'>
+        %s
+        </div>"""%open(os.path.join(path, readmefile)).read()
+    print "<div id='path'>"
+    print path
     # FIXME link only if file exist
 #    print "<a href=?download=%s>(download)</a>" % path
-    print "</td></tr>"
+    print "</div>"
 
 
 def show_dir_menu():
-    print "<table width=220px>"
-
     # print the menu
-    print "<tr><td width=220px align=right class='menu-td'><a class='menu-a' href='?'>top</a></td></tr>"
-    print "<tr><td width=220px align=right class='menu-td'><a class='menu-a' href='?path=" + urllib.quote(os.path.split(path)[0]) + "'>back</a></td></tr>"
+    print """
+    <div id='menu-dir'>
+    <span><a class='menu-a' href='?'>top</a></span>
+    <span><a class='menu-a' href='?path=%s'>back</a></span>
+    """%(urllib.quote(os.path.split(path)[0]))
     if len(dirlist):
-        print "<tr><td width=220px class='menu-td'></td></tr>"
+        print "<span></span>"
     for i in dirlist:
-        print("<tr><td width=220px class='menu-td'><a class='menu-a' href='?path=%s'>%s</a></td></tr>")%(urllib.quote(i),os.path.basename(i))
-    print "</table>"
+        print("<span><a class='menu-a' href='?path=%s'>%s</a></span>")%(urllib.quote(i),os.path.basename(i))
+    print "</div>"
 
 def show_photo_menu():
     # get and print the readme file
-    print "<table width=220px>"
+    print "<div id='menu-photo'>"
     # print the menu
-    print "<tr align=right  colspan=2><td colspan=2 class='menu-td'></td></tr>"
+    print "<span></span>"
     count = 0
-    print "<tr>"
     for i in toflist:
-        thumb = create_mini_thumb(i)
-        print("<td align=center class='menu-td' width=70px><a href='?action=%s&path=%s&file=%s'><img border=0 src='%s' alt='%s'></a><span class='toflist-text'>%s</span></td>\n"%(A_SLIDE, path, urllib.quote(os.path.basename(i)), thumb, thumb, ""))
+        thumb = create_mini_thumb(i["path"])
+        print("<span><a href='?action=%s&path=%s&file=%s'><img border=0 src='%s' alt='%s'></a><span class='toflist-text'>%s</span></span>"%(A_SLIDE, path, urllib.quote(os.path.basename(i["path"])), thumb, thumb, ""))
         count += 1
         if count == 2:
-            print "</tr><tr>"
+            print ""
             count = 0
-    print "</table>"
+    print "</div>"
 
 def print_footer():
-    print "</tr>"
-    print "<tr colspan=2><td colspan=2 class='menu-td' width='100%' align='right'>"
-    print "Blastotof v%s, <a class='menu-a' href='http://glot.net'>caf@glot.net</a>"%(version)
-    print "</td></tr></table></body></html>"
+    print """
+    <div id="footer">
+    Blastotof v%s, <a class='menu-a' href='http://glot.net'>caf@glot.net</a>
+    </div>
+    </div>
+    </body></html>
+    """%(version)
 
 def list_files():
     # build list of directory and photos
@@ -132,21 +136,22 @@ def list_files():
                 # this is a directory !!!
                 dirlist.append(os.path.normpath(pathname))
             elif os.path.isfile(pathname):
-                # this is a file, is it a jpg file ? (and not a thunmbnail file)
+                # this is a file, is it a jpg file ?
                 if re.match("(.+\.(jpg|JPG|tiff|tif)$)", f) :
-                    if debug : print "added this photo: " + pathname
-                    toflist.append(pathname)
+                    # maybe he got a desc file associated
+                    if os.path.isfile(pathname + ".txt"):
+                        desc = open(pathname + ".txt").readlines()[0]
+                    else:
+                        desc = ""
+                    tof={"path" : pathname,
+                         "desc" : desc }
+                    if debug : print "added this photo: " + str(tof)
+                    
+                    toflist.append(tof)
+
     dirlist.sort()
     toflist.sort()
 
-def read_desc_file():
-    # build the list of photos descriptions
-    global desc
-    if os.path.isfile(os.path.join(path, descfile)):
-        for l in open(os.path.join(path, descfile)).readlines():
-            tof,ds=l.split(".jpg", 1)
-            desc[path + "/" + tof + ".jpg"] = ds
-    if debug: print desc	
 
 
 def create_generic_thumb(p, prefix, size):
@@ -178,17 +183,15 @@ def create_thumb(p):
     return create_generic_thumb(p, "tn", (200, 150))
 
 def show_photo(prev, cur, next):
-    medium_thumb = create_med_thumb(cur)
-    print "<td valign=top><table><tr>"
-    print("<td width=200 align=center class='toflist-td' ><a href='%s'><img border=0 src='%s' alt='%s'></a>%s</span></td>\n" %(cur,  medium_thumb, "", ""))
-    print "</tr></table></td>"
+    medium_thumb = create_med_thumb(cur["path"])
+    print "<div id='photo'>"
+    print("<a href='%s'><img border=0 src='%s' alt='%s'></a>%s\n" %(cur["path"],  medium_thumb, cur["desc"], cur["desc"]))
+    print "</div>"
 
    
 print_headers()
 list_files()
-read_desc_file()
 show_title()
-print "<tr><td valign=top>"
 show_dir_menu()
  
 if action == A_SLIDE:
@@ -196,9 +199,8 @@ if action == A_SLIDE:
     cur  = None
     next = None
     show_photo_menu()
-    print "</td>"
     for idx in range(len(toflist)):
-        if os.path.basename(toflist[idx]) == file:
+        if os.path.basename(toflist[idx]["path"]) == file:
             cur = toflist[idx]
             if idx > 0: prev = toflist[idx-1]
             if idx < len(toflist) -1 : next = toflist[idx+1]
@@ -206,19 +208,17 @@ if action == A_SLIDE:
     print_footer()
 
 elif action == A_LIST :
-    print "</td>"
-    print "<td valign=top align=right><table><tr valign=top >"
+    print "<div id='photo-list'>"
     if toflist:
         cl=1
         for p in toflist:
             # if the thumbnails doesn't exist generate it
             #(current path must be writable by the user running the httpd)
-            thumb_file = create_thumb(p)
+            thumb_file = create_thumb(p["path"])
             # if there is not file description generate an empty one
-            if not desc.has_key(p): desc[p] = ""
-            print("<td align=center valign=center class='toflist-td' width=200px><a valign=center href='?action=%s&path=%s&file=%s'><img valign=center border=0 src='%s' alt='%s'></a><span class='toflist-text'>%s</span></td>\n"%(A_SLIDE, path,urllib.quote(os.path.basename(p)),thumb_file,desc[p],desc[p]))
+            print("<span class='toflist-td'><a valign=center href='?action=%s&path=%s&file=%s'><img valign=center border=0 src='%s' alt='%s'></a></span><span class='toflist-text'>%s</span>\n"%(A_SLIDE, path,urllib.quote(os.path.basename(p["path"])),thumb_file,p["desc"],p["desc"]))
             # we have 4 photos, jumping to new row
-            if cl%3==0: print('</tr><tr valign=top>\n')
+            if cl%3==0: print('\n')
             cl+=1
-    print "</tr></table></td>"
+    print "</div>"
     print_footer()
